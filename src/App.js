6892996bed539a030e57c0d6b1f615b430b27fd3
twelde <tsinat.weldetnesae@gmail.com>
2020-09-23 11:35:08 -0700

    Add % and -/+

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-`import React, {Component} from 'react';
+import React, {Component} from 'react';
 import './App.css'
 import SearchBar from './components/SearchBar/SearchBar'
 import Numbers from './components/Numbers/Numbers'
